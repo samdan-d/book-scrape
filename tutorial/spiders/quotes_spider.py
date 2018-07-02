8721d6b2fd48f3d7f1c915cbbaa12d7bfc291c63
Samdan Dansranbavuu <pri.samdan@gmail.com>
2018-07-02 22:28:26 +0800

    Add more

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -1,9 +1,16 @@
 import scrapy
 
-class QuoteSpider(scrapy.Spider):
-    name = 'quotes';
 
-    def start_requests(self):
-        urls = [
-            
-        ]
+class QuotesSpider(scrapy.Spider):
+    name = 'quotes'
+
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+        'http://quotes.toscrape.com/page/2/',
+    ]
+
+    def parse(self, response):
+        page = response.url.split('/')[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
